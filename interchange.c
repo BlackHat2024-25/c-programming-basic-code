11b2f8de5d8d254b1f6300da27f0e29ce84bd3d1
Hacker World <147739751+BlackHat2024-25@users.noreply.github.com>
2024-02-22 10:45:36 +0530

    Update interchange.c

--- interchange.c
+++ interchange.c
@@ -14,7 +14,7 @@ int main(){
 
     num1 = (num1 ^ num2);
     num2 = (num1 ^ num2);
-   // num1 = (num1 ^ num2);
+    num1 = (num1 ^ num2);
 
     printf("after interchange: num1 = %d, num2 = %d\n", num1,num2);
 
