d67cc7e4bf3914987a13da620c1e8f8aa5a36b1c
Hacker World <147739751+BlackHat2024-25@users.noreply.github.com>
2024-02-22 10:46:56 +0530

    Update

---
+++ switch.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main(){
+  int day;
+
+  printf("enter day number");
+  scanf("%d", &day);
+
+  switch(day)
+    {
+      case 1:
+      printf("Monday");
+      break;
+
+      case 2:
+      printf("Tuesday ");
+      break;
+
+      case 3:
+      printf("Wednesday");
+      break;
+
+      case 4:
+      printf("Thursday ");
+      break;
+
+      case 5:
+      printf("Friday ");
+      break;
+
+      case 6:
+      printf("Saturday ");
+      break;
+
+      
+      case 7:
+      printf("Sunday \n");
+      break;
+
+      default:
+      printf("error found 404 !");
+
+      
+    }
+  return 0;
+
+  
+  
+}
+
