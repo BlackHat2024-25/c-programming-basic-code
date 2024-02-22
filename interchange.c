6c1079251c1d8b7c7f8bab5f32c03b1909009c0e
Hacker World <147739751+BlackHat2024-25@users.noreply.github.com>
2024-02-22 08:55:02 +0530

    Update

---
+++ interchange.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+
+    int num1, num2;
+
+    printf("enter first number:");
+    scanf("%d",&num1);
+
+    printf("enter second number");
+    scanf("%d",&num2);
+
+    printf("before interchange : num1 = %d, num2 =%d\n",num1,num2);
+
+    num1 = (num1 ^ num2);
+    num2 = (num1 ^ num2);
+   // num1 = (num1 ^ num2);
+
+    printf("after interchange: num1 = %d, num2 = %d\n", num1,num2);
+
+    return 0;
+}
