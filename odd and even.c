d619e29b8ef67059013e8ec3133327027e804d0d
Hacker World <147739751+BlackHat2024-25@users.noreply.github.com>
2024-02-25 00:01:31 +0530

    Create odd and even.c

---
+++ odd and even.c
@@ -0,0 +1,14 @@
+// 7.	WAP in C to check whether the number is odd or even.
+
+#include<stdio.h>
+
+float main(){
+float num;
+
+printf("Enter your number :");
+scanf("%f",&num);
+
+(num / 2==0) ? printf("%f is even number",num) : printf("%f is a odd number",num);
+return 0;
+
+}
