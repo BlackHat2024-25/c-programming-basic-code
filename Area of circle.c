b3d5c96449e304cd71b2c2639b4411c8e01797b2
Hacker World <147739751+BlackHat2024-25@users.noreply.github.com>
2024-02-24 23:59:29 +0530

    Create Area of circle.c

---
+++ Area of circle.c
@@ -0,0 +1,17 @@
+// 3.	Write a program in C to find area of a circle. 	
+
+
+#include<stdio.h>
+
+int main(){
+
+    float radius, pi = 3.14;
+
+    printf("enter your value :");
+    scanf("%f%f",&radius,&pi);
+
+    printf("Area of circle is :%f", 3.14 * radius * radius);
+    return 0;
+
+}
+
